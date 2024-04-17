96760e68a99e3b87928b401e210ebf7b10efbafd
MugnusD <122955763+MugnusD@users.noreply.github.com>
2024-04-17 19:55:03 +0800

    Update SelectTabsBody.tsx
    Now selection tab can auto be scrolled to the top when changed.

--- src/features/tabs/SelectTabsBody.tsx
+++ src/features/tabs/SelectTabsBody.tsx
@@ -1,4 +1,4 @@
-import {FC} from 'react';
+import {FC, useEffect, useRef} from 'react';
 import {useSelector} from 'react-redux';
 import {selectGameItemType} from './selectTabsSlice.ts';
 import CharacterTabsContent from './tabBodyComponents/CharacterTabsContent.tsx';
@@ -6,12 +6,21 @@ import PosterTabsContent from "./tabBodyComponents/PosterTabsContent.tsx";
 
 const SelectTabsBody: FC = () => {
     const gameItemType = useSelector(selectGameItemType);
+    const ref = useRef<HTMLDivElement>(null);
+
+    // scroll to the top when change the tab
+    useEffect(() => {
+        if (ref.current) {
+            ref.current.scrollTop = 1;
+        }
+    }, [gameItemType]);
 
     return (
         <div
             className={
                 'flex flex-row flex-wrap justify-start gap-2 overflow-y-auto rounded-2xl border-2 border-light-blue-100 bg-light-blue-50 p-4'
             }
+            ref={ref}
         >
             {gameItemType === 'character' && <CharacterTabsContent />}
             {gameItemType === 'poster' && <PosterTabsContent />}
