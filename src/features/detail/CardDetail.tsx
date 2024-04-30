876adbb6fc2287d747216dd5380f866cd495448f
MugnusD <122955763+MugnusD@users.noreply.github.com>
2024-04-30 21:02:20 +0800

    Update CardDetail.tsx

--- src/features/detail/CardDetail.tsx
+++ src/features/detail/CardDetail.tsx
@@ -1,4 +1,4 @@
-import {FC} from 'react';
+import {FC, useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
 import {Typography} from "@material-tailwind/react";
 import useCharacters from "../../hooks/useCharacters.ts";
@@ -6,16 +6,34 @@ import useCharacters from "../../hooks/useCharacters.ts";
 const CardDetail: FC = () => {
     const {characters} = useCharacters();
     const {id} = useParams();
+    const [titleName, setTitleName] = useState('');
+    const [character, setCharacter] = useState<CharacterDetail>();
 
-    if (!characters)
+    useEffect(() => {
+        if (!characters) return;
+        const character = characters.find(item => item.id.toString() === id);
+
+        if (!character) return;
+        setTitleName(character.name);
+        setCharacter(character);
+    }, [characters, id]);
+
+
+    useEffect(() => {
+        if(titleName !== '') {
+            document.title = titleName;
+        }
+
+        return () => {
+            document.title = 'WDS Team Builder';
+        }
+    }, [titleName]);
+
+
+    if (!characters || !character)
         return null;
 
-    const character = characters.find(item => item.id.toString() === id);
-
-    if (!character)
-        return null;
-
-    const {name, characterBase, sense: {descriptionsChinese}, starAct: {descriptionChinese: SADesc}} = character;
+    const {name, characterBase, sense: {descriptionsChinese}, starAct: {descriptionChinese: SaDesc}} = character;
 
     return (
         <div className={'flex-col'}>
@@ -32,7 +50,7 @@ const CardDetail: FC = () => {
                 Sense: {descriptionsChinese}
             </Typography>
             <Typography>
-                Star Act: {SADesc}
+                Star Act: {SaDesc}
             </Typography>
         </div>
     )
